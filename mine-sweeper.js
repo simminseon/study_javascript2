a3e101b962a4c3047ae452eadc7767088688d52e
simminseon <alstjs7440@naver.com>
2022-10-20 17:07:28 +0900

    지뢰찾기

--- mine-sweeper.js
+++ mine-sweeper.js
@@ -4,52 +4,142 @@ const row = 10;
 const cell = 10;
 const mine = 10;
 const CODE = {
-    NORMAL: -1,
-    QUESTION: -2,
-    FLAG: -3,
-    QUESTION_MINE: -4,
-    FLAG_MINE: -5,
-    MINE: -6,
-    OPENED: 0, // 0 이상이면 오두 열린 칸
+  NORMAL: -1, // 닫힌 칸(지뢰 없음)
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  MINE: -6,
+  OPENED: 0, // 0 이상이면 오두 열린 칸
 };
 
-function plantMine() {}
-function drawTable() {
-    for (let j = 0; j < row; j++) {
-        const tr = document.createElement("tr");
-        $tbody.append(tr);
-        for (let i = 0; i < cell; i++) {
-            const td = document.createElement("td");
-            tr.appendChild(td);
-        }
-    }
-}
-
-drawTable();
-const candidate = Array(cell * row)
+function plantMine() {
+  const candidate = Array(cell * row)
     .fill()
     .map((data, i) => {
-        return i;
+      return i;
     });
-const shuffle = [];
+  const shuffle = [];
 
-while (candidate.length > row * cell - mine) {
-    const choosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+  while (candidate.length > row * cell - mine) {
+    const choosen = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1
+    )[0];
     shuffle.push(choosen);
-}
+  }
 
-console.log("shuffle", shuffle);
-const data = [];
-for (let i = 0; i < row; i++) {
+  console.log("shuffle", shuffle);
+  const data = [];
+  for (let i = 0; i < row; i++) {
     const rowData = [];
     data.push(rowData);
     for (let j = 0; j < cell; j++) {
-        // const cellData = [];
-        rowData.push(CODE.NORMAL);
+      rowData.push(CODE.NORMAL);
     }
-}
-data.map((data) => {
-    data;
-});
+  }
 
-console.log(data);
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+
+    data[ver][hor] = CODE.MINE;
+  }
+
+  console.log(data);
+  return data;
+}
+
+const onRightClick = (e) => {
+  e.preventDefault();
+  const target = e.target;
+  const rowIndex = target.parentNode.rowIndex;
+  const cellIndex = target.cellIndex;
+  const cellData = data[rowIndex][cellIndex];
+
+  if (cellData === CODE.MINE) {
+    // 지뢰면
+    data[rowIndex][cellIndex] = CODE.QUESTION_MINE; // 물음표지뢰로
+    target.className = "question";
+    target.textContent = "?";
+  } else if (cellData === CODE.QUESTION_MINE) {
+    // 물음표지뢰면
+    data[rowIndex][cellIndex] = CODE.FLAG_MINE; // 깃발지뢰로
+    target.className = "flag";
+    target.textContent = "!";
+  } else if (cellData === CODE.FLAG_MINE) {
+    // 깃발지뢰면
+    data[rowIndex][cellIndex] = CODE.MINE; // 지뢰로
+    target.className = "";
+    target.textContent = "X";
+  } else if (cellData === CODE.NORMAL) {
+    // 닫힌칸이면
+    data[rowIndex][cellIndex] = CODE.QUESTION; // 물음표로
+    target.className = "question";
+    target.textContent = "?";
+  } else if (cellData === CODE.QUESTION) {
+    // 물음표면
+    data[rowIndex][cellIndex] = CODE.FLAG; // 깃발로
+    target.className = "flag";
+    target.textContent = "!";
+  } else if (cellData === CODE.FLAG) {
+    // 깃발이면
+    data[rowIndex][cellIndex] = CODE.NORMAL; // 닫힌칸으로
+    target.className = "";
+    target.textContent = "";
+  }
+};
+
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+const countMine = (rowIndex, cellIndex) => {
+  const mines = [CODE.MINE, CODE.QUESTION_MINE, CODE.FLAG_MINE];
+  let i = 0;
+  mines.includes(data[rowIndex - 1]?.[cellIndex - 1]) && i++;
+  mines.includes(data[rowIndex - 1]?.[cellIndex]) && i++;
+  mines.includes(data[rowIndex - 1]?.[cellIndex + 1]) && i++;
+  mines.includes(data[rowIndex][cellIndex - 1]) && i++;
+  mines.includes(data[rowIndex][cellIndex + 1]) && i++;
+  mines.includes(data[rowIndex + 1]?.[cellIndex - 1]) && i++;
+  mines.includes(data[rowIndex + 1]?.[cellIndex]) && i++;
+  mines.includes(data[rowIndex + 1]?.[cellIndex + 1]) && i++;
+  return i;
+};
+
+const onLeftClick = (e) => {
+  const target = e.target;
+  const rowIndex = target.parentNode.rowIndex;
+  const cellIndex = target.cellIndex;
+  const cellData = data[rowIndex][cellIndex];
+
+  if (cellData === CODE.NORMAL) {
+    // 닫힌 칸이면
+    const count = countMine(rowIndex, cellIndex);
+    target.textContent = count || "";
+    target.className = "opened";
+    data[rowIndex][cellIndex] = count;
+  } else if (cellData === CODE.MINE) {
+    alert("탈락!!!");
+  }
+};
+
+function drawTable() {
+  data = plantMine();
+  data.forEach((row) => {
+    const $tr = document.createElement("tr");
+    row.forEach((cell) => {
+      const $td = document.createElement("td");
+      if (cell === CODE.MINE) {
+        $td.textContent = "X";
+      }
+      $tr.append($td);
+    });
+    $tbody.append($tr);
+  });
+  $tbody.addEventListener("contextmenu", onRightClick);
+  $tbody.addEventListener("click", onLeftClick);
+}
+// plantMine();
+drawTable();
